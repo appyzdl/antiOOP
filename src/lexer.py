bb858c2115d5b44eb4ef5e92ea192c5bbcc69f81
appyzdl <appyzdl5@gmail.com>
2024-08-01 15:19:01 +0530

    add: added lexer.py

---
+++ src/lexer.py
@@ -0,0 +1,84 @@
+from enum import Enum, auto
+
+
+class TokenType(Enum):
+    INTEGER = auto()
+    FLOAT = auto()
+    PLUS = auto()
+    MINUS = auto()
+    MULTIPLY = auto()
+    DIVIDE = auto()
+    LPAREN = auto()
+    RPAREN = auto()
+    EOF = auto()
+
+
+class Token:
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+
+    def __repr__(self) -> str:
+        return f"Token({self.type}, {self.value})"
+
+
+class Lexer:
+    def __init__(self, text):
+        self.text = text
+        self.pos = 0
+        self.current_char = self.text[self.pos]
+
+    def advance(self):
+        self.pos += 1
+        if self.pos > len(self.text)-1:
+            self.current_char = None
+        else:
+            self.current_char = self.text[self.pos]
+
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+
+    def integer(self) -> int:
+        result = ''
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+        return int(result)
+
+    def get_next_token(self):
+        while self.current_char is not None:
+            if self.current_char.isspace():
+                self.skip_whitespace()
+                continue
+
+            if self.current_char.isdigit():
+                return Token(TokenType.INTEGER, self.integer())
+
+            if self.current_char == '+':
+                self.advance()
+                return Token(TokenType.PLUS, '+')
+
+            if self.current_char == '-':
+                self.advance()
+                return Token(TokenType.MINUS, '-')
+
+            if self.current_char == '*':
+                self.advance()
+                return Token(TokenType.MULTIPLY, '*')
+
+            if self.current_char == '/':
+                self.advance()
+                return Token(TokenType.DIVIDE, '/')
+
+            if self.current_char == '(':
+                self.advance()
+                return Token(TokenType.LPAREN, '(')
+
+            if self.current_char == ')':
+                self.advance()
+                return Token(TokenType.RPAREN, ')')
+
+            raise Exception(f"Invalid character: {self.current_char}")
+
+        return Token(TokenType.EOF, None)
